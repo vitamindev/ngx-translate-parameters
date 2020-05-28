a9be2bbf484d1e39fb869729eaf56e21ad758d88
benoitpaul <benoit.paul@gmail.com>
2020-05-28 08:03:16 -0400

    added examples of translation in code and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   paramValue1 = 'John';
   paramValue2 = 'Patricia';
+
+  stringInCode: string;
+
+  constructor(private translate: TranslateService) {}
+
+  async ngOnInit() {
+    await this.translate.use('en').toPromise();
+    this.stringInCode = this.translate.instant('TEXT', {
+      value1: this.paramValue1,
+      value2: this.paramValue2,
+    });
+  }
 }
